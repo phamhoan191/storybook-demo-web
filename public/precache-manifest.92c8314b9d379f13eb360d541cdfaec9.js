294cb8e4435ac215034c18fab4f6b1bff6e61a4e
phamhoan191 <phamhoan191@gmail.com>
2020-08-24 16:50:41 +0700

    Create storybook demo web

---
+++ public/precache-manifest.92c8314b9d379f13eb360d541cdfaec9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50841c844184830ab23ff7235220574",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.496faf9ec352be258e3c.bundle.js"
+  },
+  {
+    "url": "runtime~main.496faf9ec352be258e3c.bundle.js"
+  },
+  {
+    "revision": "0439879041ee88572866",
+    "url": "static/css/main.424912e5.chunk.css"
+  },
+  {
+    "url": "vendors~main.496faf9ec352be258e3c.bundle.js"
+  },
+  {
+    "url": "vendors~main.496faf9ec352be258e3c.bundle.js.LICENSE.txt"
+  }
+]);
